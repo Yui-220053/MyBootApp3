0d6ab017f0700db4e0cde9e45fadd47ad7ffa555
Yui-220053 <te220053@gmail.com>
2025-05-20 12:05:19 +0900

    commit

--- src/main/java/jp/te4a/spring/boot/myapp5/mybootapp5/HelloController.java
+++ src/main/java/jp/te4a/spring/boot/myapp5/mybootapp5/HelloController.java
@@ -1,17 +1,19 @@
 package jp.te4a.spring.boot.myapp5.mybootapp5;
 import org.springframework.web.bind.annotation.RestController;
- import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
  /**
  * Hello world!
  */
- @RestController
+ @Controller
 public class HelloController {
-    @RequestMapping("/")
- public String index() {
-  return "Hello, Spring Boot!";
- }
- @RequestMapping("/miyagi")
- public String miyagi() {
-  return "Miyagi desu!";
- }
+        @RequestMapping("/")
+    public String index(Model model) {
+    //return "this is Spring Boot sample.";
+    return "index";
+     }
+    @RequestMapping("/miyagi")
+     public String miyagi() {
+     return "Miyagi desu!";
+     }
 }
